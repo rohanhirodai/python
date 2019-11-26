2ff269807af560dac91ffbdcb10912dd60d396df
Anwarul_Hirodai <56622913+rohanhirodai@users.noreply.github.com>
2019-11-26 18:22:51 +0900

    Forms using Python Pyramid framework
    Store and retrieve data using the SQLAlchemy ORM atop the SQLite database. Our Pyramid-based application now needs database-backed storage of pages. This frequently means an SQL database. The Pyramid community strongly supports the SQLAlchemy project and its object-relational mapper (ORM) as a convenient, Pythonic way to interface to databases. In this application, using SQLAlchemy to a SQLite database table, we provide storage and retrieval for the pages.

---
+++ pa_projects/pa_python/forms/paproject/tests.py
@@ -0,0 +1,67 @@
+import unittest
+
+from pyramid import testing
+
+
+class ProjectViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_home(self):
+        from .views import WikiViews
+
+        request = testing.DummyRequest()
+        inst = WikiViews(request)
+        response = inst.wiki_view()
+        self.assertEqual(len(response['pages']), 3)
+
+
+class ProjectFunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from paproject import main
+
+        app = main({})
+        from webtest import TestApp
+
+        self.testapp = TestApp(app)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_home(self):
+        res = self.testapp.get('/', status=200)
+        self.assertIn(b'<title>Wiki: View</title>', res.body)
+
+    def test_add_page(self):
+        res = self.testapp.get('/add', status=200)
+        self.assertIn(b'<h1>Wiki</h1>', res.body)
+
+    def test_edit_page(self):
+        res = self.testapp.get('/101/edit', status=200)
+        self.assertIn(b'<h1>Wiki</h1>', res.body)
+
+    def test_post_wiki(self):
+        self.testapp.post('/add', {
+            "title": "New Title",
+            "body": "<p>New Body</p>",
+            "submit": "submit"
+        }, status=302)
+
+        res = self.testapp.get('/103', status=200)
+        self.assertIn(b'<h1>New Title</h1>', res.body)
+        self.assertIn(b'<p>New Body</p>', res.body)
+
+    def test_edit_wiki(self):
+        self.testapp.post('/102/edit', {
+            "title": "New Title",
+            "body": "<p>New Body</p>",
+            "submit": "submit"
+        }, status=302)
+
+        res = self.testapp.get('/102', status=200)
+        self.assertIn(b'<h1>New Title</h1>', res.body)
+        self.assertIn(b'<p>New Body</p>', res.body)
+
