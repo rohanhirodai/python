181dd9bef8278320e5e8d59430985f66bb5e14c9
Anwarul_Hirodai <56622913+rohanhirodai@users.noreply.github.com>
2020-01-09 17:47:09 +0900

    Tenplating using jinja2
    Pyramid does not prefer one templating language over another. Jinja2 is a popular templating system. Here objective is to show Pyramid's support for different templating systems.

---
+++ jinja2/paproject/views.py
@@ -0,0 +1,21 @@
+from pyramid.view import(
+
+	view_config,
+	view_defaults
+
+	)
+
+@view_defaults(renderer='home.jinja2')
+class projectviews:
+
+	def __init__(self,request):
+		self.request=request
+
+	@view_config(route_name='home')
+	def home(self):
+		return{'name':'Home View'}
+
+	@view_config(route_name='hello')
+	def hello(self):
+		return{'name':'Hello View'}
+
---
+++ jinja2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+requires=[
+
+	'pyramid',
+	'pyramid_chameleon',
+	'pyramid_jinja2',
+	'waitress',
+]
+
+dev_requires=[
+	'pyramid_debugtoolbar',
+	'pytest',
+	'webtest',
+
+]
+
+setup(
+	name='paproject',
+	install_requires=requires,
+	extras_require={
+		'dev':dev_requires,
+	},
+	entry_points={
+		'paste.app_factory':[
+			'main=paproject:main'
+
+		],
+
+
+	},
+
+
+)
