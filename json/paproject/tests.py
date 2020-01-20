dbdc494d820d1a36a1d27a8373760e160a6066d2
Anwarul_Hirodai <56622913+rohanhirodai@users.noreply.github.com>
2020-01-20 15:09:33 +0900

    AJAX development
    It is a pyramid application with JASON renderer. Output from the view is then run through the renderer, which generates and returns the response. Chameleon renderer used here. Pyramid supplies a JSON renderer which takes Python data, serializes it to JSON.

---
+++ json/paproject/tests.py
@@ -0,0 +1,58 @@
+import unittest
+
+from pyramid import testing
+
+class ProjectViewTests(unittest.TestCase):
+	def setUp(self):
+		self.config=testing.setUp()
+
+	def tearDown(self):
+		testing.tearDown()
+
+
+
+	def test_home(self):
+		from .views import projectviews 
+	
+		request=testing.DummyRequest()
+		inst=projectviews(request)
+		response=inst.home()
+		self.assertEqual('Home View',response['name'])
+
+
+	def test_hello(self):
+		from .views import projectviews 
+	
+		request=testing.DummyRequest()
+		inst=projectviews(request)
+		response=inst.hello()
+		self.assertEqual('Hello View',response['name'])
+
+	
+
+class ProjectFunctionalTests(unittest.TestCase):
+	def setUp(self):
+		from paproject import main
+		app=main({})
+		from webtest import TestApp
+
+		self.testapp=TestApp(app)
+
+
+	def test_home(self):
+		res=self.testapp.get('/',status=200)
+		self.assertIn(b'<h1>Hi Home View',res.body)
+
+
+	def test_hello(self):
+		res=self.testapp.get('/howdy',status=200)
+		self.assertIn(b'<h1>Hi Hello View',res.body)
+
+
+	def test_hello_json(self):
+		res=self.testapp.get('/howdy.json',status=200)
+		self.assertIn(b'{"name": "Hello View"}',res.body)
+		self.assertEqual(res.content_type,'application/json')
+
+
+	
