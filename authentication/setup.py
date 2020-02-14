2a0cc2e771ac978962347982156b48a17d2b616e
Anwarul_Hirodai <56622913+rohanhirodai@users.noreply.github.com>
2020-02-14 15:20:38 +0900

    Logins with authentication
    It is about login views that authenticate a username and password against a list of users. It will add security to the application. We will introduce authentication first using Pyramid's rich facilities for pluggable user storage and then we will introduce protection of resources with authorization security statements.

---
+++ authentication/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+requires=[
+	'bcrypt',
+	'pyramid',
+	'pyramid_chameleon',
+	'waitress',
+]
+
+dev_requires=[
+	'pyramid_debugtoolbar',
+	'pytest',
+	'webtest',
+
+]
+
+setup(
+	name='paproject',
+	install_requires=requires,
+	extras_require={
+		'dev':dev_requires,
+	},
+	entry_points={
+		'paste.app_factory':[
+			'main=paproject:main'
+
+		],
+
+
+	},
+
+
+)
