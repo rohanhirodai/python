dab8d6b5c4937c1e4c3a371bdd079e9ab1f8f342
Anwarul_Hirodai <56622913+rohanhirodai@users.noreply.github.com>
2020-02-21 14:57:24 +0900

    Views With Routing
    It is dispatching URLs To Views With Routing. Routing matches incoming URL patterns to view code. Pyramid's routing has a number of useful features. Here, I will define a route that extracts part of the URL into a Python dictionary. Then I use that dictionary data in a view.

---
+++ routing/paproject/views.py
@@ -0,0 +1,23 @@
+from pyramid.view import(
+
+	view_config,
+	view_defaults
+
+	)
+
+@view_defaults(renderer='home.pt')
+class projectviews:
+
+	def __init__(self,request):
+		self.request=request
+
+	@view_config(route_name='home')
+	def home(self):
+		first=self.request.matchdict['first']
+		last=self.request.matchdict['last']
+		return{
+		'name':'Home View',
+		'first':first,
+		'last':last
+		}
+
